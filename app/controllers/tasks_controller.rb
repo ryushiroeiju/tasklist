69e75990d86ec4b3a3fd2f2cd2ceecb7a5ef5882
ryushiro.eiju <l.j2323to6@gmail.com>
2017-04-25 01:34:24 +0000

    5th commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,4 @@
-class MessagesController < ApplicationController
+class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
@@ -13,6 +13,14 @@ class MessagesController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+    if@task.save
+      flash[:success] = 'Taskが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskが投稿されませんでした'
+      render :new
+    end
+  
   end
 
   def edit
@@ -20,8 +28,26 @@ class MessagesController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'Taskは正常に更新されました'
+      redirect_to @task
+    else 
+      flash.now[:danger] = 'Taskは更新されませんでした'
+      render :edit 
+    end
   end
 
   def destroy
+     @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  private
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
